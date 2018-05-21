b01d2d1a92c97bf1a7f3517cb5a0a6661e69044c
Shin <542782758@qq.com>
2018-05-21 21:53:48 +0800

    addddd

--- Apriori-hw5/Apriori.py
+++ Apriori-hw5/Apriori.py
@@ -1,119 +1,106 @@
 from itertools import chain, combinations
 from collections import defaultdict
 
-def subsets(arr):
-    """ Returns non empty subsets of arr"""
-    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+class Apriori(object):
+    def __init__(self, f_name, sup=0.1, con=0.1):
+        self.data = self._read_csv(f_name)
+        self.sup = sup
+        self.con = con
+        self.items = []
+        self.rules = []
 
-def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
-        """calculates the support for items in the itemSet and returns a subset
-       of the itemSet each of whose elements satisfies the minimum support"""
-        _itemSet = set()
-        localSet = defaultdict(int)
+    def _read_csv(self, f_name):
+        with open(f_name, 'r') as f:
+            for line in f:
+                line = line.strip().rstrip(',')
+                item = frozenset(line.split(','))
+                yield item
 
-        for item in itemSet:
-                for transaction in transactionList:
-                        if item.issubset(transaction):
-                                freqSet[item] += 1
-                                localSet[item] += 1
+    def run(self):
+        # 小工具函数
+        def _get_support(item):
+            return float(freq_set[item]) / len(deals)
+        def _get_subsets(item):
+            return chain(*[combinations(item, i + 1) for i, a in enumerate(item)])
+        def _join_set(item_set, length):
+            return set([i.union(j) for i in item_set for j in item_set if len(i.union(j)) == length])
 
-        for item, count in localSet.items():
-                support = float(count)/len(transactionList)
+        # 初始化 L1 项集和交易数据
+        item_set, deals = self._init_data()
 
-                if support >= minSupport:
-                        _itemSet.add(item)
+        freq_set = defaultdict(int)
+        large_set = dict()
 
-        return _itemSet
+        init_set = self._remove_item_set(item_set, deals, freq_set)
+        l_set = init_set
 
-def joinSet(itemSet, length):
-        """Join a set with itself and returns the n-element itemsets"""
-        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+        k = 2
+        while (l_set != set([])):
+            large_set[k - 1] = l_set
+            l_set = _join_set(l_set, k)
+            c_set = self._remove_item_set(l_set, deals, freq_set)
+            l_set = c_set
+            k += 1
 
-def getItemSetTransactionList(data_iterator):
-    transactionList = list()
-    itemSet = set()
-    for record in data_iterator:
-        transaction = frozenset(record)
-        transactionList.append(transaction)
-        for item in transaction:
-            itemSet.add(frozenset([item]))              # Generate 1-itemSets
-    return itemSet, transactionList
+        # 组合结果
+        for key, value in large_set.items():
+            self.items.extend([(tuple(item), _get_support(item)) for item in value])
 
-def runApriori(data_iter, minSupport, minConfidence):
-    """
-    run the apriori algorithm. data_iter is a record iterator
-    Return both:
-     - items (tuple, support)
-     - rules ((pretuple, posttuple), confidence)
-    """
-    itemSet, transactionList = getItemSetTransactionList(data_iter)
+        for key, value in list(large_set.items())[1:]:
+            for item in value:
+                _subsets = map(frozenset, [x for x in _get_subsets(item)])
+                for element in _subsets:
+                    remain = item.difference(element)
+                    if len(remain) > 0:
+                        con = _get_support(item) / _get_support(element)
+                        if con >= self.con:
+                            self.rules.append(((tuple(element), tuple(remain)), con))
 
-    freqSet = defaultdict(int)
-    largeSet = dict()
-    # Global dictionary which stores (key=n-itemSets,value=support)
-    # which satisfy minSupport
+    def _init_data(self):
+        deal_list = list()
+        item_set = set()
 
-    assocRules = dict()
-    # Dictionary which stores Association Rules
+        for d in self.data:
+            deal = frozenset(d)
+            deal_list.append(deal)
 
-    oneCSet = returnItemsWithMinSupport(itemSet,
-                                        transactionList,
-                                        minSupport,
-                                        freqSet)
+            # 产生 L1 项集
+            for item in deal:
+                item_set.add(frozenset([item]))
 
-    currentLSet = oneCSet
-    k = 2
-    while(currentLSet != set([])):
-        largeSet[k-1] = currentLSet
-        currentLSet = joinSet(currentLSet, k)
-        currentCSet = returnItemsWithMinSupport(currentLSet,
-                                                transactionList,
-                                                minSupport,
-                                                freqSet)
-        currentLSet = currentCSet
-        k = k + 1
+        return item_set, deal_list
 
-    def getSupport(item):
-            """local function which Returns the support of an item"""
-            return float(freqSet[item])/len(transactionList)
+    # 根据支持度阈值删除项集
+    def _remove_item_set(self, item_set, deals, freq_set):
+        res = set()
+        local_set = defaultdict(int)
 
-    toRetItems = []
-    for key, value in largeSet.items():
-        toRetItems.extend([(tuple(item), getSupport(item))
-                           for item in value])
+        # 统计
+        for item in item_set:
+            for d in deals:
+                if item.issubset(d):
+                    freq_set[item] += 1
+                    local_set[item] += 1
 
-    toRetRules = []
-    for key, value in list(largeSet.items())[1:]:
-        for item in value:
-            _subsets = map(frozenset, [x for x in subsets(item)])
-            for element in _subsets:
-                remain = item.difference(element)
-                if len(remain) > 0:
-                    confidence = getSupport(item)/getSupport(element)
-                    if confidence >= minConfidence:
-                        toRetRules.append(((tuple(element), tuple(remain)),
-                                           confidence))
-    return toRetItems, toRetRules
+        # 删除
+        for item, count in local_set.items():
+            support = float(count) / len(deals)
+            if support >= self.sup:
+                res.add(item)
 
-def printResults(items, rules):
-    """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
-    #for item, support in sorted(items, key=lambda item, support: support):
-    print(u'------频繁项集------')
-    for item, support in sorted(items, key=lambda items: items[1]):
-        print ('%s , %.2f' % (str(item), support))
-    print (u'\n------关联规则------')
-    for rule, confidence in sorted(rules, key=lambda rules: rules[1]):
-        pre, post = rule
-        print ('%s --> %s , %.2f' % (str(pre), str(post), confidence))
+        return res
 
-def dataFromFile(fname):
-        """Function which reads from the file and yields a generator"""
-        file_iter = open(fname, 'rU')
-        for line in file_iter:
-                line = line.strip().rstrip(',')                         # Remove trailing comma
-                record = frozenset(line.split(','))
-                yield record
+    # 输出结果
+    def show(self):
+        print(u'------频繁项集------')
+        for item, sup in sorted(self.items, key=lambda items: items[1]):
+            print ('%s , %.2f' % (str(item), sup))
+        print (u'\n------关联规则------')
+        for rule, con in sorted(self.rules, key=lambda rules: rules[1]):
+            pre, post = rule
+            print ('%s --> %s , %.2f' % (str(pre), str(post), con))
 
 if __name__ == "__main__":
-    items, rules = runApriori(dataFromFile('data.csv'), minSupport=0.15, minConfidence=0.6)
-    printResults(items, rules)
+    a = Apriori('groceries.csv', 0.05, 0.2)
+    a.run()
+    a.show()
