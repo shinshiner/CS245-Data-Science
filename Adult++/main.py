d710820077f49dd5d2620aef8b4e19637cdee15d
Shin <542782758@qq.com>
2018-06-12 15:06:54 +0800

    ddl.......

--- Adult++/main.py
+++ Adult++/main.py
@@ -8,6 +8,8 @@ def norm(data):
     scaler.fit(data)
     return scaler.transform(data)
 
+random_seed = 666
+np.random.seed(666)
 # load data
 X = np.load('adult_x.npy')
 Y = np.load('adult_y.npy')
@@ -17,19 +19,21 @@ x_t = np.load('adult_x_t.npy')
 y_tr = np.load('adult_y_tr.npy')
 y_t = np.load('adult_y_t.npy')
 
-interval = 150
+interval = 3000
 max_iter = 3000
 res_tr = []
 res_t = []
 
+x = [0.00001, 0.00003, 0.00006, 0.0001, 0.0003, 0.0006, 0.001, 0.003, 0.006, 0.01]
 # training stage
-for i in range(interval, max_iter + 1, interval):
-#     model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-#             decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-#             max_iter=i, probability=False, shrinking=True,
-#             tol=0.001, verbose=False, random_state=random_seed)
-    model = SVC(kernel='poly', random_state=random_seed)
-    model.fit(x_tr, y_tr)
+for tmp in x:
+    for i in range(interval, max_iter + 1, interval):
+    #     model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #             decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    #             max_iter=i, probability=False, shrinking=True,
+    #             tol=0.001, verbose=False, random_state=random_seed)
+        model = SVC(kernel='rbf', gamma=tmp, random_state=random_seed)
+        model.fit(x_tr, y_tr)
 
     y_pre_tr = model.predict(x_tr)
     y_pre_t = model.predict(x_t)
